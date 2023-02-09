cc3f78ac6a53ee1d79c37de16bb9fead0006fd4c
bilnik <hoflandbilly@gmail.com>
2023-02-09 11:34:13 +0100

    initial commit

---
+++ Individual project/Circusclasses/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circusclasses
+{
+    public enum Size
+    {
+        small = 1,
+        medium = 3,
+        large = 5,
+    }
+    public enum Type
+    {
+        Carnivore, 
+        Herbivore
+    }
+    public class Animal
+    {
+        private string name;
+        private Size size;
+        private Type type;
+        private static int Animalcount = 0;
+
+      
+        public Animal(string name, Size size, Type type)
+        {
+            this.name = name;
+            this.size = size;
+            this.type = type;
+           
+        }
+        public Animal(Size size, Type type ) : this("Nameless animal " + Animalcount.ToString(), size, type)
+        {
+            
+        }
+        public int IncrementAnimal()
+        {
+            return Animalcount++;
+        }
+
+        public override string ToString()
+        {
+            return name.ToString() + " , " + size.ToString() + " , " + type.ToString();
+        }
+    }
+}
---
+++ Individual project/Circusclasses/Wagon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circusclasses
+{
+    
+    public class Wagon
+    {
+        private const int capacity = 10;
+
+        private List<string> animals = new List<string>();
+
+
+
+    }
+}
